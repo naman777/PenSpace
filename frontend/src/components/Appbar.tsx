979cff2ef75f32bec2863a5ff148def1a936fe98
Naman Kundra <namankundra6@gmail.com>
2024-05-08 18:59:41 +0530

    profile section added

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -21,9 +21,30 @@ export const Appbar = () => {
                 navigate("/")
             }  } className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Log Out</button>
 
-            <Avatar size={"big"} name={email?.toUpperCase()}  onClick={()=>{
+            <Avatar2 size={"big"} name={email?.toUpperCase()}  onClick={()=>{
                 navigate("/profile")
             }}/>
         </div>
     </div>
 }
+
+ function Avatar2({
+    name,
+    size = "small",
+    onClick
+  }: {
+    name: any,
+    size?: "small" | "big",
+    onClick?: () => void
+  }) {
+    return (
+      <div 
+        className={`relative inline-flex items-center justify-center overflow-hidden bg-gray-600 rounded-full ${size === "small" ? "w-6 h-6" : "w-10 h-10"}`}
+        onClick={onClick}
+      >
+        <span className={`${size === "small" ? "text-xs" : "text-md"} font-extralight text-gray-600 dark:text-gray-300`}>
+          {name[0]}
+        </span>
+      </div>
+    );
+  }
